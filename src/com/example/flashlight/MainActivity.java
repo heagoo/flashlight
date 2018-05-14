911790670a00fb5c49706d3398407a767f8d90e6
heagoo <heagoo@gmail.com>
2018-05-14 23:00:44 +0800

    Update MainActivity.java
    compile error fix

--- src/com/example/flashlight/MainActivity.java
+++ src/com/example/flashlight/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.flashlight;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
